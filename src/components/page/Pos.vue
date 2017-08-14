df2ff219f149c6e969c6d791564e6576811f45d3
李云渊 <975663670@qq.com>
2017-08-14 22:06:17 +0800

    0814

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -53,9 +53,39 @@
 									</ul>
 	  						</div>
 	  					</el-tab-pane>
-	  					<el-tab-pane label="饮料"></el-tab-pane>
-	  					<el-tab-pane label="小吃"></el-tab-pane>
-	  					<el-tab-pane label="套餐"></el-tab-pane>
+	  					<el-tab-pane label="饮料">
+	  						<div>
+									<ul class='cookList'>
+									    <li v-for="goods in type1Goods">
+									        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+									        <span class="foodName">{{goods.goodsName}}</span>
+									        <span class="foodPrice">￥{{goods.price}}元</span>
+									    </li>
+									</ul>
+	  						</div>
+	  					</el-tab-pane>
+	  					<el-tab-pane label="小吃">
+	  						<div>
+									<ul class='cookList'>
+									    <li v-for="goods in type2Goods">
+									        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+									        <span class="foodName">{{goods.goodsName}}</span>
+									        <span class="foodPrice">￥{{goods.price}}元</span>
+									    </li>
+									</ul>
+	  						</div>
+	  					</el-tab-pane>
+	  					<el-tab-pane label="套餐">
+	  					    <div>
+									<ul class='cookList'>
+									    <li v-for="goods in type3Goods">
+									        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+									        <span class="foodName">{{goods.goodsName}}</span>
+									        <span class="foodPrice">￥{{goods.price}}元</span>
+									    </li>
+									</ul>
+	  						</div>
+	  					</el-tab-pane>
 	  				</el-tabs>
 	  			</div>
 	  			
@@ -68,130 +98,63 @@
 </template>
 
 <script>
+    import axios from 'axios';	
 export default {
 	data(){
 		return{
 			tabData:[
-			  {
-          goodsName: '可口可乐',
-          price: 8,
-          count:1
-        }, {
-          
-          goodsName: '香辣鸡腿堡',
-          price: 15,
-          count:1
-        }, {
-         
-          goodsName: '爱心薯条',
-          price: 8,
-          count:1
-        }, {
-         
-          goodsName: '甜筒',
-          price: 8,
-          count:1
-        }
-      ],
-      oftenGoods:[
-          {
-              goodsId:1,
-              goodsName:'香辣鸡腿堡',
-              price:18
-          }, {
-              goodsId:2,
-              goodsName:'田园鸡腿堡',
-              price:15
-          }, {
-              goodsId:3,
-              goodsName:'和风汉堡',
-              price:15
-          }, {
-              goodsId:4,
-              goodsName:'快乐全家桶',
-              price:80
-          }, {
-              goodsId:5,
-              goodsName:'脆皮炸鸡腿',
-              price:10
-          }, {
-              goodsId:6,
-              goodsName:'魔法鸡块',
-              price:20
-          }, {
-              goodsId:7,
-              goodsName:'可乐大杯',
-              price:10
-          }, {
-              goodsId:8,
-              goodsName:'雪顶咖啡',
-              price:18
-          }, {
-              goodsId:9,
-              goodsName:'大块鸡米花',
-              price:15
-          }, {
-              goodsId:20,
-              goodsName:'香脆鸡柳',
-              price:17
-          }
-      ],
-      type0Goods:[
-          {
-              goodsId:1,
-              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-              goodsName:'香辣鸡腿堡',
-              price:18
-          }, {
-              goodsId:2,
-              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-              goodsName:'田园鸡腿堡',
-              price:15
-          }, {
-              goodsId:3,
-              goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
-              goodsName:'和风汉堡',
-              price:15
-          }, {
-              goodsId:4,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-              goodsName:'快乐全家桶',
-              price:80
-          }, {
-              goodsId:5,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-              goodsName:'脆皮炸鸡腿',
-              price:10
-          }, {
-              goodsId:6,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos004.jpg",
-              goodsName:'魔法鸡块',
-              price:20
-          }, {
-              goodsId:7,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos001.jpg",
-              goodsName:'可乐大杯',
-              price:10
-          }, {
-              goodsId:8,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos003.jpg",
-              goodsName:'雪顶咖啡',
-              price:18
-          }, {
-              goodsId:9,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-              goodsName:'大块鸡米花',
-              price:15
-          }, {
-              goodsId:20,
-               goodsImg:"http://7xjyw1.com1.z0.glb.clouddn.com/pos002.jpg",
-              goodsName:'香脆鸡柳',
-              price:17
-          }
-          
-      ],
-        
+			    {
+		          goodsName: '可口可乐',
+		          price: 8,
+		          count:1
+		        }, {
+		          
+		          goodsName: '香辣鸡腿堡',
+		          price: 15,
+		          count:1
+		        }, {
+		         
+		          goodsName: '爱心薯条',
+		          price: 8,
+		          count:1
+		        }, {
+		         
+		          goodsName: '甜筒',
+		          price: 8,
+		          count:1
+		        }
+		    ],
+		    oftenGoods:[],
+		    type0Goods:[],
+		    type1Goods:[],
+		    type2Goods:[],
+		    type3Goods:[]
 		}
+	},
+	created(){
+		axios.get('http://jspang.com/DemoApi/oftenGoods.php')
+		.then((respons)=>{
+			//console.log(respons);
+			this.oftenGoods = respons.data
+		})
+		.catch((err)=>{
+			console.log(err);
+			alert('获取数据失败！')
+		});
+		axios.get('http://jspang.com/DemoApi/typeGoods.php')
+		.then((respons)=>{
+			console.log(respons);
+			this.type0Goods = respons.data[0];
+			this.type1Goods = respons.data[1];
+			this.type2Goods = respons.data[2];
+			this.type3Goods = respons.data[3];
+		})
+		.catch((err)=>{
+			console.log(err);
+			alert('获取数据失败！')
+		});
+		
+		
 	},
 	mounted(){
 		var docuHei = document.body.clientHeight;
